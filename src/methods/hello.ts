ebfd7bd49da1727cbe11e9cdf6548478735d6d58
Neetshin <neetshin@neetsh.in>
2018-02-20 17:00:47 +0530

    Implement basic greeting feature

--- src/methods/hello.ts
+++ src/methods/hello.ts
@@ -1,8 +1,16 @@
-import TelegramBot, { Message } from 'node-telegram-bot-api';
+import { Message } from 'node-telegram-bot-api';
 import { __ } from 'i18n';
 
-const hello = (message: Message): string => {
-  return  __('hello');
-}
+const hello = (message: Message): Promise<string> => {
+  return new Promise((resolve) => {
+    const { from } = message;
+
+    if (from && from.first_name) {
+      resolve(__('hello_with_name', from.first_name));
+    }
+
+    resolve(__('hello'));
+  });
+};
 
 export default hello;
