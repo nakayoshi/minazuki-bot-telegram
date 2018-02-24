e9d3bc1cc9ade487291acb92202e5b62f6b9f4c1
Neetshin <neetshin@neetsh.in>
2018-02-24 12:38:43 +0530

    Add permission detection for leave cmd

--- src/methods/leave.ts
+++ src/methods/leave.ts
@@ -1,18 +1,27 @@
-import TelegramBot, { Message } from 'node-telegram-bot-api';
+import TelegramBot, { ChatMember } from 'node-telegram-bot-api';
 import { __ } from 'i18n';
 
-const leave = (api: TelegramBot, message: Message): Promise<Message | boolean | Error> => {
+const leave = (api: TelegramBot, message: TelegramBot.Message): Promise<TelegramBot.Message | boolean | Error> => {
   return new Promise((resolve) => {
-    const { chat } = message;
+    const { chat, from } = message;
 
     if ( chat.type !== 'group' && chat.type !== 'supergroup' ) {
       resolve(api.sendMessage(chat.id, __('leave_on_private')));
       return;
     }
 
-    api.sendMessage(chat.id, __('leave')).then(() => {
-      resolve(api.leaveChat(chat.id));
-    });
+    if ( chat.id && from && from.id ) {
+      api.getChatMember(chat.id, `${from.id}`).then((chatMember) => {
+        if (chatMember instanceof Error || !chatMember.can_restrict_members) {
+          resolve(api.sendMessage(chat.id, __('leave_permission_denied')));
+          return;
+        }
+
+        api.sendMessage(chat.id, __('leave')).then(() => {
+          resolve(api.leaveChat(chat.id));
+        });
+      });
+    }
   });
 };
 
