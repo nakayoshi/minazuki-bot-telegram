312e71eacff564ec67e1a6ecba7ac444aad89867
Neetshin <neetshin@neetsh.in>
2018-04-10 19:12:40 +0530

    Make breakable in md command

--- src/methods/md.ts
+++ src/methods/md.ts
@@ -4,7 +4,7 @@ import { __ } from 'i18n';
 const md = (api: TelegramBot, message: Message): Promise<Message | Error> => {
   return new Promise((resolve) => {
     const { chat, text = '' } = message;
-    const matches = text.match(/^\/md\s(.+?)$/);
+    const matches = text.match(/^\/md\s([.\n]+?)$/);
 
     if (!matches) {
       resolve(api.sendMessage(chat.id, __('md_undefined')));
